a5e9b2b01a26b998a31ae3983a9515cbdee5a6fc
manojrnaik <61896458+manojrnaik@users.noreply.github.com>
2025-12-14 11:37:56 +0530

    Create login_screen.dart

---
+++ mobile_app/lib/features/auth/screens/login_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import '../../../shared/widgets/primary_button.dart';
+import '../../../core/router/app_router.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            const TextField(
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            const SizedBox(height: 12),
+            const TextField(
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'Password'),
+            ),
+            const SizedBox(height: 24),
+            PrimaryButton(
+              text: 'Login',
+              onPressed: () {
+                Navigator.pushReplacementNamed(
+                  context,
+                  AppRouter.home,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
