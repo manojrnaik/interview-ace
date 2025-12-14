17d673818aef43d68cc28ddb2c05068dd8c405f7
manojrnaik <61896458+manojrnaik@users.noreply.github.com>
2025-12-14 11:37:21 +0530

    Create primary_button.dart

---
+++ mobile_app/lib/shared/widgets/primary_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class PrimaryButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const PrimaryButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        child: Padding(
+          padding: const EdgeInsets.all(14),
+          child: Text(text),
+        ),
+      ),
+    );
+  }
+}
