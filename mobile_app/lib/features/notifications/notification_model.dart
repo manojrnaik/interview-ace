a38b2c57e600cc6b2036cf74fb6709bca521d21a
manojrnaik <61896458+manojrnaik@users.noreply.github.com>
2025-12-14 13:50:39 +0530

    Create notification_model.dart

---
+++ mobile_app/lib/features/notifications/notification_model.dart
@@ -0,0 +1,13 @@
+class InterviewNotification {
+  final String id;
+  final String title;
+  final String body;
+  final DateTime scheduledTime;
+
+  InterviewNotification({
+    required this.id,
+    required this.title,
+    required this.body,
+    required this.scheduledTime,
+  });
+}
